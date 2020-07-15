88ca9c79cd5152bd1fb67ec6ee9be62449004c5e
Adam Michaud <amichaud982@gmail.com>
2020-07-15 15:27:50 -0500

    End of Chapter 23; Added if statements, logical operators, while statements, and for statements

--- compiler.c
+++ compiler.c
@@ -498,6 +498,57 @@ static void expressionStatement(void)
     emitByte(OP_POP);
 }
 
+static void forStatement(void)
+{
+    beginScope();
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+    if (match(TOKEN_SEMICOLON)) {
+        // No initailizer.
+    }
+    else if (match(TOKEN_VAR))
+        varDeclaration();
+    else
+        expressionStatement();
+
+    int loopStart = currentChunk()->count;
+
+    int exitJump = -1;
+
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+        // Jump out of the loop if the condition is false.
+        exitJump = emitJump(OP_JUMP_IF_FALSE);
+        emitByte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int bodyJump = emitJump(OP_JUMP);
+
+        int incrementStart = currentChunk()->count;
+        expression();
+        emitByte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emitLoop(loopStart);
+        loopStart = incrementStart;
+        patchJump(bodyJump);
+    }
+
+    statement();
+
+    emitLoop(loopStart);
+
+    if (exitJump != -1) {
+        patchJump(exitJump);
+        emitByte(OP_POP);
+    }
+
+    endScope();
+}
+
 static void ifStatement(void)
 {
     consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
@@ -584,6 +635,8 @@ static void statement(void)
 {
     if (match(TOKEN_PRINT))
         printStatement();
+    else if (match(TOKEN_FOR))
+        forStatement();
     else if (match(TOKEN_IF))
         ifStatement();
     else if (match(TOKEN_WHILE))

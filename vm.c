1e6c5bf4e6db8a839aad7f3c1f2c804feb970462
Adam Michaud <amichaud982@gmail.com>
2020-07-21 22:03:52 -0500

    End of Chapter 27.2; Support for declaring, accessing, and invoking methods have been added to Lox

--- vm.c
+++ vm.c
@@ -140,6 +140,11 @@ static bool callValue(Value callee, int argCount)
 {
     if (IS_OBJ(callee)) {
         switch (OBJ_TYPE(callee)) {
+            case OBJ_BOUND_METHOD:
+            {
+                ObjBoundMethod *bound = AS_BOUND_METHOD(callee);
+                return call(bound->method, argCount);
+            }
 			case OBJ_CLASS:
 			{
 				ObjClass *klass = AS_CLASS(callee);
@@ -164,6 +169,20 @@ static bool callValue(Value callee, int argCount)
 
     runtimeError("Can only call functions and classes.");
     return false;
+}
+
+static bool bindMethod(ObjClass *klass, ObjString *name)
+{
+    Value method;
+    if (!tableGet(&klass->methods, name, &method)) {
+        runtimeError("Undefined property '%s'.", name->chars);
+        return false;
+    }
+
+    ObjBoundMethod *bound = newBoundMethod(peek(0), AS_CLOSURE(method));
+    pop();
+    push(OBJ_VAL(bound));
+    return true;
 }
 
 static ObjUpvalue *captureUpvalue(Value *local)
@@ -198,6 +217,14 @@ static void closeUpvalues(Value *last)
         upvalue->location = &upvalue->closed;
         vm.openUpvalues = upvalue->next;
     }
+}
+
+static void defineMethod(ObjString *name)
+{
+    Value method = peek(0);
+    ObjClass *klass = AS_CLASS(peek(1));
+    tableSet(&klass->methods, name, method);
+    pop();
 }
 
 static inline bool isFalsey(Value value)
@@ -335,8 +362,9 @@ static InterpretResult run(void)
 					break;
 				}
 
-				runtimeError("Undefined property '%s'.", name->chars);
-				return INTERPRET_RUNTIME_ERROR;
+                if (!bindMethod(instance->klass, name))
+                    return INTERPRET_RUNTIME_ERROR;
+                break;
 			}
 			case OP_SET_PROPERTY:
 			{
@@ -468,6 +496,9 @@ static InterpretResult run(void)
 			}
         case OP_CLASS:
             push(OBJ_VAL(newClass(READ_STRING())));
+            break;
+        case OP_METHOD:
+            defineMethod(READ_STRING());
             break;
 		}
 	}
